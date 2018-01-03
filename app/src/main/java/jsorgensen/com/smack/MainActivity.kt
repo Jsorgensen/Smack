87e99d10a65ff4e22cce885188ac52df4db36caf
Mike Jorgensen <mikeljorgensen@gmail.com>
2018-01-03 14:18:49 -0700

    removed layout file warnings

--- app/src/main/java/jsorgensen/com/smack/MainActivity.kt
+++ app/src/main/java/jsorgensen/com/smack/MainActivity.kt
@@ -1,13 +1,9 @@
 package jsorgensen.com.smack
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
-import android.support.design.widget.NavigationView
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*

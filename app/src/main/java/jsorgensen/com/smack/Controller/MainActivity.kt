d977649184eb2d9e185b255103838979b799382c
Mike Jorgensen <mikeljorgensen@gmail.com>
2018-01-05 12:05:20 -0700

    Added Web Socket!  exciting stuff

--- app/src/main/java/jsorgensen/com/smack/Controller/MainActivity.kt
+++ app/src/main/java/jsorgensen/com/smack/Controller/MainActivity.kt
@@ -1,6 +1,7 @@
 package jsorgensen.com.smack.Controller
 
 import BROADCAST_USER_DATA_CHANGE
+import SOCKET_URL
 import android.content.*
 import android.graphics.Color
 import android.os.Bundle
@@ -9,34 +10,48 @@ import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
-import android.view.LayoutInflater
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.Toast
+import io.socket.client.IO
 import jsorgensen.com.smack.R
 import jsorgensen.com.smack.Services.AuthService
 import jsorgensen.com.smack.Services.UserDataService
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
+import kotlinx.android.synthetic.main.content_main.*
 import kotlinx.android.synthetic.main.nav_header_main.*
 
 class MainActivity : AppCompatActivity(){
+
+    val socket = IO.socket(SOCKET_URL)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        hideKeyboard()
-
         val toggle = ActionBarDrawerToggle(
                 this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
         drawer_layout.addDrawerListener(toggle)
         toggle.syncState()
+    }
 
+    override fun onResume() {
         LocalBroadcastManager.getInstance(this).registerReceiver(userDataChangeReceiver, IntentFilter(BROADCAST_USER_DATA_CHANGE))
+        socket.connect()
+        super.onResume()
+    }
+
+    override fun onPause() {
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(userDataChangeReceiver)
+        super.onPause()
+    }
+
+    override fun onDestroy() {
+        socket.disconnect()
+        super.onDestroy()
     }
 
     private val userDataChangeReceiver = object : BroadcastReceiver(){
@@ -92,14 +107,21 @@ class MainActivity : AppCompatActivity(){
                     val channelName = nameEditText.text.toString()
                     val channelDescription = descriptionEditText.text.toString()
 
-                    hideKeyboard()
+                    socket.emit("newChannel", channelName, channelDescription)
                 }.setNegativeButton("CANCEL") { dialogInterface, i ->
-                    hideKeyboard()
+
                 }.show()
     }
 
     fun onSendMessageButtonClick(view: View){
+        hideKeyboard()
 
+        if(!AuthService.isLoggedIn){
+            Toast.makeText(this, "Please login to send message.", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        messageEditText.text.clear()
     }
 
     fun hideKeyboard(){
--- app/src/main/java/jsorgensen/com/smack/Utilities/Constants.kt
+++ app/src/main/java/jsorgensen/com/smack/Utilities/Constants.kt
@@ -3,6 +3,7 @@
  */
 
 const val BASE_URL = "https://jsorgensen-chat.herokuapp.com/v1/"
+const val SOCKET_URL = "https://jsorgensen-chat.herokuapp.com/"
 const val URL_REGISTER = "${BASE_URL}account/register"
 const val URL_LOGIN = "${BASE_URL}account/login"
 const val URL_CREATE_USER = "${BASE_URL}user/add"
